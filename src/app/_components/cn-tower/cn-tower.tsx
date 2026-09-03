7a9326487343b1d9bd30f3da0d1ef1b24071ea59
Jovan <jovanmedford@gmail.com>
2026-09-02 20:54:44 -0400

    chore: adjust sizing for large screens

--- src/app/_components/cn-tower/cn-tower.tsx
+++ src/app/_components/cn-tower/cn-tower.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 
 
 export function CnTower({className}: {className?: string}) {
-    return (<div className={className}>
-        <Image alt="cn tower" src="/simple-cn-tower.svg" height={270} width={188} />
-    </div>)
+    return (
+        <Image
+            className={className}
+            alt="cn tower"
+            src="/simple-cn-tower.svg"
+            height={270}
+            width={196}
+        />
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,17 @@ export default function Home() {
         <Header />
         <section className="mt-14 mx-auto  flex flex-col md:grid md:grid-cols-12">
           <div
-            className="mt-10 md:col-start-3 md:col-end-10"
+            className="mt-10 md:col-start-3 md:col-end-9"
           >
             <div className="lg:text-left">
-              <h1 className="text-2xl lg:text-5xl font-bold">
+              <h1 className="text-2xl lg:text-5xl 2xl:text-6xl font-bold">
                 Hey, I'm Jovan Medford
               </h1>
-              <h2 className="text-lg mt-1 lg:text-3xl mb-1">
+              <h2 className="text-lg mt-1 lg:text-3xl 2xl:text-4xl mb-1">
                 I’m a software engineer based in Toronto.
               </h2>
             </div>
-            <p className="mt-8 max-w-sm">
+            <p className="mt-6 max-w-sm xl:max-w-xl xl:text-md">
               I write about my journey and things I’ve learned in my{" "}
               <a className="underline text-primary-300" href="https://medium.com/@jovanmedford">
                 blog
@@ -31,7 +31,7 @@ export default function Home() {
               .
             </p>
           </div>
-          <CnTower className="md:col-span-3 w-fit mx-auto mt-8 md:mx-0 md:mt-0" />
+          <CnTower className="md:col-span-4 w-auto mx-auto mt-8 md:mx-0 md:mt-0 h-72 lg:h-80 xl:h-96" />
         </section>
       </main>
     </>
